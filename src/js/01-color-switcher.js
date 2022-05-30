d5bae112524ab8f1002a2044ffd61498be5b02c2
Denis Lazarenko <dentus25@gmail.com>
2022-05-30 17:16:53 +0300

    add_01_color_switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,17 +1,32 @@
 'use strict';
 
 const bodyEl = document.querySelector('body');
-
 const startBtn = document.querySelector('button[data-start]');
-
 const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
 
-bodyEl.addEventListener('click', onClickStartBtn);
+bodyEl.addEventListener('click', onChangeBgColorBody);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-function onClickStartBtn(e) {
-  console.log(e.target);
+function onChangeBgColorBody(e) {
+  const { target, currentTarget } = e;
+  if (target.type !== 'button') {
+    return;
+  }
+
+  if (target === startBtn) {
+    target.setAttribute('disabled', true);
+    stopBtn.removeAttribute('disabled');
+    timerId = setInterval(
+      () => (currentTarget.style.backgroundColor = getRandomHexColor()),
+      1000
+    );
+  } else {
+    target.setAttribute('disabled', true);
+    startBtn.removeAttribute('disabled');
+    clearInterval(timerId);
+  }
 }
