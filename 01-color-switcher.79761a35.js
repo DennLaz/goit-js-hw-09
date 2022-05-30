64ad5fbd6277b29763dbe16eb0286e6552470af7
DennLaz <dentus25@gmail.com>
2022-05-30 14:18:00 +0000

    Deploying to gh-pages from @ DennLaz/goit-js-hw-09@d5bae112524ab8f1002a2044ffd61498be5b02c2 🚀

--- 01-color-switcher.79761a35.js
+++
@@ -1,2 +0,0 @@
-"use strict";var bodyEl=document.querySelector("body"),startBtn=document.querySelector("button[data-start]"),stopBtn=document.querySelector("button[data-stop]");function getRandomHexColor(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}function onClickStartBtn(t){console.log(t.target)}bodyEl.addEventListener("click",onClickStartBtn);
-//# sourceMappingURL=01-color-switcher.79761a35.js.map
