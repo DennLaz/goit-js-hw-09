aad9a0ee39d9a18e859340186534dcde367c6ec4
Denis Lazarenko <dentus25@gmail.com>
2022-05-30 13:34:44 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
+'use strict';
 
+const bodyEl = document.querySelector('body');
+
+const startBtn = document.querySelector('button[data-start]');
+
+const stopBtn = document.querySelector('button[data-stop]');
+
+bodyEl.addEventListener('click', onClickStartBtn);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onClickStartBtn(e) {
+  console.log(e.target);
+}
